e497e23ec6714f26035a36b451112b2ed33ab0e0
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2023-09-21 00:11:07 +0200

    Implement entropy tree calculator

--- src/bin/wordle.rs
+++ src/bin/wordle.rs
@@ -5,6 +5,8 @@ use std::collections::HashSet;
 use std::convert::TryInto;
 use std::io;
 use std::str::FromStr;
+use itertools::Itertools;
+use ordered_float::NotNan;
 
 const CHARACTERS_PER_WORD: usize = 6;
 
@@ -209,6 +211,165 @@ impl WordSolver {
     fn update(&mut self, guess: &str, feedback: &[CharacterFeedback; CHARACTERS_PER_WORD]) {
         let ruleset = RuleSet::from_guess_feedback(guess, feedback);
         self.dictionary.retain(|word| ruleset.satisfies(word))
+    }
+}
+
+#[derive(Copy, Clone)]
+enum PartitionResult {
+    Exact,
+    Misplaced,
+    Missing,
+}
+
+struct Word {
+    characters: [char; CHARACTERS_PER_WORD],
+}
+
+impl Word {
+    fn new(characters: [char; CHARACTERS_PER_WORD]) -> Self {
+        Self { characters }
+    }
+
+    fn from_str(s: &str) -> Self {
+        let characters: [char; CHARACTERS_PER_WORD] =
+            s
+                .chars()
+                .collect_vec()
+                .try_into()
+                .unwrap();
+        Self::new(characters)
+    }
+
+    fn partition(&self, chr: char, idx: usize) -> PartitionResult {
+        if self.characters[idx] == chr {
+            PartitionResult::Exact
+        } else if self.characters.contains(&chr) {
+            PartitionResult::Misplaced
+        } else {
+            PartitionResult::Missing
+        }
+    }
+
+    fn iter(&self) -> impl Iterator<Item=char> {
+        self.characters.into_iter()
+    }
+}
+
+
+
+struct PartitionBy<'a> {
+    exact: Vec<&'a Word>,
+    misplaced: Vec<&'a Word>,
+    missing: Vec<&'a Word>,
+}
+
+impl<'a> PartitionBy<'a> {
+    fn partition(words: &[&'a Word], chr: char, idx: usize) -> PartitionBy<'a> {
+        let mut exact: Vec<&'a Word> = Vec::new();
+        let mut misplaced: Vec<&'a Word> = Vec::new();
+        let mut missing: Vec<&'a Word> = Vec::new();
+
+        for &word in words {
+            match word.partition(chr, idx) {
+                PartitionResult::Exact => exact.push(word),
+                PartitionResult::Misplaced => misplaced.push(word),
+                PartitionResult::Missing => missing.push(word)
+            }
+        }
+
+        Self { exact, misplaced, missing }
+    }
+
+    fn split(self) -> (Vec<&'a Word>, Vec<&'a Word>, Vec<&'a Word>) {
+        (self.exact, self.misplaced, self.missing)
+    }
+}
+
+fn entropy(probabilities: &[f32]) -> f32 {
+    let s: f32 = probabilities
+        .iter()
+        .copied()
+        .map(|p| p * p.log2())
+        .sum();
+    -s
+}
+
+enum DictionaryTreeNode<'a> {
+    Leaf(Vec<&'a Word>),
+    PartitionedBy {
+        chr: char,
+        idx: usize,
+        exact: Box<DictionaryTreeNode<'a>>,
+        misplaced: Box<DictionaryTreeNode<'a>>,
+        missing: Box<DictionaryTreeNode<'a>>,
+    },
+}
+
+impl<'a> DictionaryTreeNode<'a> {
+    fn leaf_sizes(&self) -> Vec<usize> {
+        match self {
+            Self::Leaf(words) => vec![words.len()],
+            Self::PartitionedBy { exact, misplaced, missing, .. } => {
+                [exact, misplaced, missing]
+                    .into_iter()
+                    .flat_map(|tree| tree.leaf_sizes())
+                    .collect_vec()
+            }
+        }
+    }
+
+    fn entropy(&self) -> f32 {
+        let sizes = self.leaf_sizes();
+        let total_count: usize = sizes.iter().sum();
+        if total_count == 0 {
+            return 0.0;
+        }
+
+        let probabilities = sizes
+            .into_iter()
+            .map(|size| (size as f32) / (total_count as f32))
+            .collect_vec();
+
+        entropy(&probabilities)
+    }
+
+    fn partition(words: Vec<&'a Word>, idx: usize, open_characters: &HashSet<char>) -> DictionaryTreeNode<'a> {
+        if idx == CHARACTERS_PER_WORD {
+            DictionaryTreeNode::Leaf(words)
+        } else {
+            open_characters
+                .iter()
+                .copied()
+                .map(|chr| {
+                    let (exact, misplaced, missing) = PartitionBy::partition(&words, chr, idx).split();
+                    let exact = Self::partition(exact, idx + 1, open_characters);
+                    let misplaced = Self::partition(misplaced, idx + 1, open_characters);
+                    let missing = Self::partition(missing, idx + 1, open_characters);
+
+                    DictionaryTreeNode::PartitionedBy {
+                        idx,
+                        chr,
+                        exact: Box::new(exact),
+                        misplaced: Box::new(misplaced),
+                        missing: Box::new(missing),
+                    }
+                })
+                .max_by_key(|node| NotNan::new(node.entropy()).unwrap())
+                .unwrap()
+        }
+    }
+
+    fn from_words(words: Vec<&'a Word>) -> Self {
+        let open_characters = words
+            .iter()
+            .fold(HashSet::new(), |mut acc, word|{
+                for chr in word.iter() {
+                    acc.insert(chr);
+                }
+                acc
+            });
+
+        Self::partition(words, 0, &open_characters)
     }
 }
 
